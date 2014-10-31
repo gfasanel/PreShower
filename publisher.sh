fd87f250653dddbe923923666f28cdc1ffb608a4
gfasanel <gfasanel@Amerigo.(none)>
2014-10-31 22:11:37 +0100

    with publisher

---
+++ publisher.sh
@@ -0,0 +1,5 @@
+scp -r plots/combined/ gfasanel@lxplus.cern.ch:~/scratch1/www/Zprime/Preshower_study/plots/
+scp gfasanel@lxplus.cern.ch:~/scratch1/www/index.php gfasanel@lxplus.cern.ch:~/scratch1/www/Zprime/Preshower_study/plots/combined
+#scp gfasanel@lxplus.cern.ch:~/scratch1/www/index.php gfasanel@lxplus.cern.ch:~/scratch1/www/Zprime/Preshower_study/plots/combined
+#scp gfasanel@lxplus.cern.ch:~/scratch1/www/index.php gfasanel@lxplus.cern.ch:~/scratch1/www/Zprime/Preshower_study/plots/combined/ROC
+#scp gfasanel@lxplus.cern.ch:~/scratch1/www/index.php gfasanel@lxplus.cern.ch:~/scratch1/www/Zprime/Preshower_study/plots/combined/ROC/ROC2/
